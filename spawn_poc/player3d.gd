69273213a5ddff71d9b5b76e9842cb52977fe085
davidenisu <david@zeni-su.com>
2025-04-15 21:26:52 +0200

    Support gyroscope rotation

--- spawn_poc/player3d.gd
+++ spawn_poc/player3d.gd
@@ -46,4 +46,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
+	var rotation = Input.get_gyroscope()
+	rotate_y(rotation.z)
+
 	move_and_slide()
